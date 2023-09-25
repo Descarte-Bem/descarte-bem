2741da0e4299bfedf47cc0a5409bfe7bfec192b1
MarcosFernando <marcosfernando@id.uff.br>
2023-09-25 18:39:26 -0300

    login funcionando sem o getx

--- lib/ui/views/login_page.dart
+++ lib/ui/views/login_page.dart
@@ -1,28 +1,19 @@
-import 'package:decarte_bem/controllers/login_controller.dart';
+import 'package:decarte_bem/ui/widgets/login_button.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
-
+  // TODO: checar se o usuário está logado ao iniciar o app
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<LoginController>(
-        init: Get.put(LoginController()),
-        builder: (loginController) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Login'),
-              centerTitle: true,
-            ),
-            body: Center(
-              child: ElevatedButton(
-                onPressed: loginController.signInWithGoogle,
-                child: const Text("Google Login"),
-              ),
-            )
-          );
-        }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Login'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: LoginButton(),
+        )
     );
   }
 }
