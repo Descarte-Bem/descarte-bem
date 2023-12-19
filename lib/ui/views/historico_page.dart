2b7f02578a1f5278c06fa86d30fb747658b7129b
vinimg <viniciusmg@id.uff.br>
2023-12-19 00:56:44 -0300

    historico de descarte

--- lib/ui/views/historico_page.dart
+++ lib/ui/views/historico_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../../models/descarte_model.dart';
 
 class HistoricoPage extends StatefulWidget {
   const HistoricoPage({Key? key}) : super(key: key);
@@ -8,16 +11,41 @@ class HistoricoPage extends StatefulWidget {
 }
 
 class _HistoricoPageState extends State<HistoricoPage> {
+  List<DescarteModel> discardList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getDiscards();
+  }
+
+  getDiscards() async {
+    discardList = await FirebaseFirestore.instance
+        .collection('descartes')
+        .where('usuario', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((value) =>
+            value.docs.map(DescarteModel.fromQueryDocSnapshot).toList());
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body:Center(
-        child: Text('Historico',
-        style: TextStyle(
-          fontSize: 20,
-          )
-        )
-     ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Histórico de Descartes'),
+      ),
+      body: ListView.builder(
+        itemCount: discardList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(discardList[index]
+                .usuarioId), // replace with your actual data field
+            subtitle: Text(discardList[index].farmaciaId ??
+                'No FarmaciaId'), // replace with your actual data field
+          );
+        },
+      ),
     );
   }
 }
